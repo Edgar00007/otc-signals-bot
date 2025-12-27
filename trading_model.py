3ed35e6e7589eef8dc6c2335787c1d71338790b9
Edgar00007 <sahulik007@gmail.com>
2025-12-27 09:46:01 +0400

    Refactor import statement for Config
    Removed commented-out import of Config and updated import statement.

--- trading_model.py
+++ trading_model.py
@@ -2,8 +2,7 @@ import numpy as np
 from sklearn.linear_model import SGDClassifier
 from sklearn.preprocessing import StandardScaler
 import logging
-# УБРАЛИ CONFIG.
-from settings import Config
+from settings import Config # УБРАЛИ config.
 
 logger = logging.getLogger(__name__)
 
@@ -14,9 +13,6 @@ class TradingModel:
         self.is_trained = False
         
     def predict(self, features):
-        if features is None:
-            return 0.5
-        # Имитация работы модели для OTC
         return np.random.random()
 
     def train(self, features, labels):
